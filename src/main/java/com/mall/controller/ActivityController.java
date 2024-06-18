84275efd01caa38f13a778b2e80474f147919211
wangyz <2829555018@qq.com>
2024-06-18 14:02:26 +0800

    整理

---
+++ src/main/java/com/mall/controller/ActivityController.java
@@ -0,0 +1,18 @@
+package com.mall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/activity")
+public class ActivityController {
+
+    @GetMapping("/index")
+    public String test(Model model){
+        model.addAttribute("name","张三");
+        return "index";
+    }
+
+}
